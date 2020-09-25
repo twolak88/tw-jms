4fe009f85ab24caa9251cf79f57557c492b5b6f9
twolak88 <twolak88@gmail.com>
2020-09-25 22:29:18 +0200

    initial project jms artemis

---
+++ src/main/java/com/twolak/springframework/twjms/TwJmsApplication.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.twjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twolak/springframework/twjms/TwJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.twjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
