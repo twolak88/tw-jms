884692f00cda41d0cfef8a47a09f7469a42f904c
twolak88 <twolak88@gmail.com>
2020-09-26 09:46:34 +0200

    simple JMS server configuration

--- src/main/java/com/twolak/springframework/twjms/TwJmsApplication.java
+++ src/main/java/com/twolak/springframework/twjms/TwJmsApplication.java
@@ -1,12 +1,22 @@
 package com.twolak.springframework.twjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwJmsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+				.setPersistenceEnabled(false)
+				.setJournalDirectory("target/data/journal")
+				.setSecurityEnabled(false)
+				.addAcceptorConfiguration("invm", "vm://0"));
+		activeMQServer.start();
+		
 		SpringApplication.run(TwJmsApplication.class, args);
 	}
 
