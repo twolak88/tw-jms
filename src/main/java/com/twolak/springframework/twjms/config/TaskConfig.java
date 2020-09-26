f18ef1a10e50b87c79976c3c4ed46faa73aea419
twolak88 <twolak88@gmail.com>
2020-09-26 09:59:12 +0200

    Task Configuration

---
+++ src/main/java/com/twolak/springframework/twjms/config/TaskConfig.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.twolak.springframework.twjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author twolak
+ *
+ */
+@EnableScheduling
+@EnableAsync
+@Configuration
+public class TaskConfig {
+	
+	@Bean
+	TaskExecutor taskExecutor() {
+		return new SimpleAsyncTaskExecutor();
+	}
+}
